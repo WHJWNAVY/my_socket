c09b7ad230245ea6d0e11b9d45559841d289cb40
root <root@wnavy-vpc.(none)>
2017-09-04 09:25:39 +0800

    first version v20170904

---
+++ build_client.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: build_client.sh
+# Author: wnavy
+# mail: whjwnavy@163.com
+# Created Time: Fri 01 Sep 2017 03:35:43 PM CST
+#########################################################################
+#!/bin/bash
+gcc test_client.c my_socket.c -o test_client
---
+++ build_server.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: build_server.sh
+# Author: wnavy
+# mail: whjwnavy@163.com
+# Created Time: Fri 01 Sep 2017 03:33:59 PM CST
+#########################################################################
+#!/bin/bash
+gcc test_server.c my_socket.c -o test_server
